d9ca6cd3ca33b540b99b9ba8c42956485516f375
Bhavy Airi <airiragahv@gmail.com>
2023-09-13 21:13:01 +0530

    Fix contact name page

--- components/team/MainTeam.tsx
+++ components/team/MainTeam.tsx
@@ -8,10 +8,10 @@ const MainContent: React.FC = () => {
         <div className="flex flex-wrap justify-center text-center mb-24 mt-24">
         <div className="w-full lg:w-6/12 px-4">
           <h1 className="text-white-900 text-4xl font-bold mb-8">
-            Our team <b className='text-green-700 '>Flora Finder</b>
+            Our team <b className='text-green-700 '>Coder Cops</b>
           </h1>
           <p className="text-red-500 text-lg font-light">
-            With over 100 years of combined experience, we&apos;ve got a well-seasoned team at the helm.
+            With some combined experience, we&apos;ve got a well-seasoned team at the helm.
           </p>
         </div>
       </div>
