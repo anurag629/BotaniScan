121f3114cb0e8e3a9ff6c7eab5ac6053c0d85d52
anurag629 <akvermaav629@gmail.com>
2023-09-13 11:09:52 +0530

    adde the mentor and team positon at first

--- components/team/Team.tsx
+++ components/team/Team.tsx
@@ -4,6 +4,8 @@ import TeamMember from './TeamMember';
 const Team: React.FC = () => {
     const teamDetails = [
         {
+            isTeamLeader: false,
+            isMentor: false,
             name: "Akansha", 
             MemberPosition: "(Team Member)",
             title: "C++ and Web Developer",
@@ -16,6 +18,8 @@ const Team: React.FC = () => {
             description: "Hello How are",
         },
         {
+            isTeamLeader: false,
+            isMentor: false,
             name: "Rukshali",
             MemberPosition: "(Team Member)",
             title: "Python Programmer",
@@ -28,6 +32,8 @@ const Team: React.FC = () => {
             description: "Hello How are",
         },
         {
+            isTeamLeader: false,
+            isMentor: false,
             name: "Bhavy Airi",
             MemberPosition: "(Team Member)",
             title: "Web Developer",
@@ -40,6 +46,8 @@ const Team: React.FC = () => {
             description: "With over 100 years of combined experience, we've got a well-seasoned team at the helm.",
         },
         {
+            isTeamLeader: false,
+            isMentor: false,
             name: "Abhishek Gupta",
             MemberPosition: "(Team Member)",
             title: "Python & Web Developer",
@@ -52,10 +60,12 @@ const Team: React.FC = () => {
             description: "Have a strong background in python and c++. I am passionate about finding bugs in code and transforming ideas into reality. Have experience in cybersecurity and Machine learning",
         },
         {
+            isTeamLeader: false,
+            isMentor: false,
             name: "Subrat",
             MemberPosition: "(Team Member)",
             title: "Programmer & Web Developer",
-            imageSrc: "/assets/profile/subrat.jpeg",
+            imageSrc: "/assets/profile/subrat.png",
             linkedinUrl: "https://www.linkedin.com/in/subratyadav/",
             twitterUrl: "https://twitter.com/findingsubrat?t=hUHGhRBlKT2ZKJfcmPXaGQ&s=09",
             githubUrl: "https://github.com/Subrat29",
@@ -64,6 +74,8 @@ const Team: React.FC = () => {
             description: "I have a strong background in C++ and Java and am currently learning web development.",
         },
         {
+            isTeamLeader: true,
+            isMentor: false,
             name: "Anurag Verma",
             MemberPosition: "(Team Leader)",
             title: "AI/ML & Web Developer",
@@ -75,13 +87,29 @@ const Team: React.FC = () => {
             email: "akvermaav629@gmail.com",
             description: "Have a strong background in math, statistics, and programming. I am passionate about translating complex data into actionable insights. Have experience in building machine learning & deep learning models. I quick learner and enjoy working in a fast-paced work environment.",
         },
+        {
+            isTeamLeader: false,
+            isMentor: true,
+            name: "Ravi",
+            MemberPosition: "(Mentor)",
+            title: "Python & Web Developer",
+            imageSrc: "/assets/profile/woman.png",
+            linkedinUrl: "https://www.linkedin.com/in/ravi-kumar-37463/",
+            twitterUrl: "https://twitter.com/anurag_629",
+            githubUrl: "https://github.com/anurag629",
+            instagramUrl: "https://www.instagram.com/anurag_629/",
+            email: "akvermaav629@gmail.com",
+            description: "Have a strong background in math, statistics, and programming. I am passionate about translating complex data into actionable insights. Have experience in building machine learning & deep learning models. I quick learner and enjoy working in a fast-paced work environment.",
+        },
     ];
 
     return (
-        <div className="flex flex-wrap flex justify-center">
+        <div className="flex flex-wrap justify-center">
             {teamDetails.map((member, index) => (
                 <TeamMember
                     key={index}
+                    isTeamLeader={member.isTeamLeader}
+                    isMentor={member.isMentor}
                     MemberPosition={member.MemberPosition}
                     name={member.name}
                     title={member.title}
--- components/team/TeamMember.tsx
+++ components/team/TeamMember.tsx
@@ -1,90 +1,89 @@
-// TeamMember.tsx (Server Component)
 'use client' // 👈 use it here
-
 // TeamMember.tsx
+ 
+
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faTwitter, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import Image from 'next/image';
-import { Tooltip, Typography } from "@material-tailwind/react";
 
-
-interface TeamMemberProps {
-    name: string;
-    MemberPosition: string;
-    title: string;
-    linkedinUrl: string;
-    twitterUrl: string;
-    githubUrl: string;
-    instagramUrl: string;
-    imageSrc: string;
-    description: string;
-    email: string;
+export interface TeamMemberProps {
+  isTeamLeader: boolean;
+  isMentor: boolean;
+  name: string;
+  MemberPosition: string;
+  title: string;
+  linkedinUrl: string;
+  twitterUrl: string;
+  githubUrl: string;
+  instagramUrl: string;
+  imageSrc: string;
+  description: string;
+  email: string;
 }
 
 const TeamMember: React.FC<TeamMemberProps> = ({
-    name,
-    MemberPosition,
-    title,
-    linkedinUrl,
-    twitterUrl,
-    githubUrl,
-    instagramUrl,
-    imageSrc,
-    description,
-    email,
+  isTeamLeader,
+  isMentor,
+  name,
+  MemberPosition,
+  title,
+  linkedinUrl,
+  twitterUrl,
+  githubUrl,
+  instagramUrl,
+  imageSrc,
+  description,
+  email,
 }) => {
-
-
-    return (
-        <div
-            className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4 hover:scale-110 transition-transform duration-300"        >
-            <div className="flex flex-col">
-                    <div>
-                        <Image
-                            className="rounded-2xl drop-shadow-md"
-                            src={imageSrc}
-                            alt={name}
-                            width={300} 
-                            height={200} 
-                        />
-                    </div>
-                    <div className="text-center mt-6">
-                        <h1 className="text-white-900 text-xl font-bold mb-1">{name}</h1>
-                        <div className="text-red-500 font-light mb-2">{title}</div>
-                        <div className="text-red-500 font-light mb-2">{MemberPosition}</div>      
-                    </div>
-            
-                <div className="flex items-center justify-center">
-                    <a href={linkedinUrl} target="_blank" rel="noopener noreferrer" className="flex rounded-full h-10 w-10 hover:scale-150 transition-transform duration-300">
-                        <FontAwesomeIcon icon={faLinkedin} className="text-blue-500 mx-auto mt-2" />
-                    </a>
-                    <a href={twitterUrl} target="_blank" rel="noopener noreferrer" className="flex rounded-full h-10 w-10 hover:scale-150 transition-transform duration-300">
-                        <FontAwesomeIcon icon={faTwitter} className="text-blue-400 mx-auto mt-2" />
-                    </a>
-                    <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="flex rounded-full h-10 w-10 hover:scale-150 transition-transform duration-300">
-                        <FontAwesomeIcon icon={faGithub} className="text-gray-600 mx-auto mt-2" />
-                    </a>
-                    <a href={instagramUrl} target="_blank" rel="noopener noreferrer" className="flex rounded-full h-10 w-10 hover:scale-150 transition-transform duration-300">
-                        <FontAwesomeIcon icon={faInstagram} className="text-pink-500 mx-auto mt-2" />
-                    </a>
-
-
-
-
-                </div>
-            </div>
-            {/* Render the MemberDetailsPopup conditionally */}
-            {/* {isHovered && (
-                <MemberDetailsPopup
-                    name={name}
-                    title={title}
-                    description={description}
-                    email={email}              
-                />
-            )} */}
+  return (
+    <div
+      className={`w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4 hover:scale-110 transition-transform duration-300 ${
+        isTeamLeader ? 'order-first' : ''
+      }`}
+    >
+      <div className="flex flex-col">
+        <div>
+          <Image
+            className="rounded-2xl drop-shadow-md"
+            src={imageSrc}
+            alt={name}
+            width={300}
+            height={200}
+          />
         </div>
-    );
+        <div className="text-center mt-6">
+          <h1 className="text-white-900 text-xl font-bold mb-1">{name}</h1>
+          <div className="text-red-500 font-light mb-2">{title}</div>
+          <div className="dark:text-gray-300 font-light mb-2">{MemberPosition}</div>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <a href={linkedinUrl} target="_blank" rel="noopener noreferrer" className="flex rounded-full h-7 w-7 hover:scale-150 transition-transform duration-300">
+            <FontAwesomeIcon icon={faLinkedin} className="text-blue-500 mx-auto mt-2" />
+          </a>
+          <a href={twitterUrl} target="_blank" rel="noopener noreferrer" className="flex rounded-full h-7 w-7 hover:scale-150 transition-transform duration-300">
+            <FontAwesomeIcon icon={faTwitter} className="text-blue-400 mx-auto mt-2" />
+          </a>
+          <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="flex rounded-full h-7 w-7 hover:scale-150 transition-transform duration-300">
+            <FontAwesomeIcon icon={faGithub} className="text-gray-600 mx-auto mt-2" />
+          </a>
+          <a href={instagramUrl} target="_blank" rel="noopener noreferrer" className="flex rounded-full h-7 w-7 hover:scale-150 transition-transform duration-300">
+            <FontAwesomeIcon icon={faInstagram} className="text-pink-500 mx-auto mt-2" />
+          </a>
+        </div>
+      </div>
+      {/* Render the MemberDetailsPopup conditionally */}
+      {/* {isHovered && (
+        <MemberDetailsPopup
+          name={name}
+          title={title}
+          description={description}
+          email={email}              
+        />
+      )} */}
+    </div>
+  );
 };
 
 export default TeamMember;
