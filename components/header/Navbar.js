9fbcd198cc06a8ab33c37d06bc01d729ee482b33
anurag629 <akvermaav629@gmail.com>
2023-09-18 13:42:31 +0530

    updated and fixed imports

--- components/header/Navbar.js
+++ components/header/Navbar.js
@@ -1,9 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
-import { RxCross1 } from "react-icons/rx";
-import Link from "next/link";
-import Image from "next/image";
+
 import DropdownMenu from "./DropDownMenu";
 
 const Navbar = () => {
@@ -12,13 +9,6 @@ const Navbar = () => {
   const toggleTransformation = () => {
     setIstransformed(!isTransformed);
   }
-
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
 
   const menu = [
     { name: "Home", url: "/" },
